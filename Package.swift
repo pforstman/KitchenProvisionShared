39344f95f746333fc0a0920ac254202191a27363
Paul Forstman <pforstman@gmail.com>
2026-04-28 07:41:12 -0500

    init: KitchenProvisionShared package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KitchenProvisionShared",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "KitchenProvisionShared",
+            targets: ["KitchenProvisionShared"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "KitchenProvisionShared"
+        ),
+        .testTarget(
+            name: "KitchenProvisionSharedTests",
+            dependencies: ["KitchenProvisionShared"]
+        ),
+    ]
+)
